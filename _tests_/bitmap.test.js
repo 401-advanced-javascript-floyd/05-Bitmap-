b3c49a8481972bfa8551d715d64b36dabe61284e
Floyd Orr <floydcanhelp@gmail.com>
2019-05-27 10:19:50 -0500

    fix eslint

--- _tests_/bitmap.test.js
+++ _tests_/bitmap.test.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const fs = require("fs");
-const util = require("util");
+const fs = require('fs');
+const util = require('util');
 const fsReadFile = util.promisify(fs.readFile);
 
-const Bitmap = require("../lib/bitmap");
+const Bitmap = require('../lib/bitmap');
 
  const bmpPath = `${__dirname}/../assets/baldy.bmp`;
 
-describe("Bitmap Module", () => {
- it("can parse a file!", async () => {
+describe('Bitmap Module', () => {
+ it('can parse a file!', async () => {
    const buffer = await fsReadFile(bmpPath);
    expect(buffer).toBeDefined();
 
@@ -18,10 +18,10 @@ describe("Bitmap Module", () => {
    bitmap.parse(buffer);
 
    expect(bitmap.buffer).toBe(buffer);
-   expect(bitmap.type).toBe("BM");
-   expect(bitmap.size).toBe(2780604);
-   expect(bitmap.pixels).toBe(16);
-   expect(bitmap.width).toBe(1237);
-   expect(bitmap.offset).toBe(54);
+   expect(bitmap.type).toBe('BM');
+   expect(bitmap.size).toBe(15146);
+   expect(bitmap.pixels).toBe(8);
+   expect(bitmap.width).toBe(110);
+   expect(bitmap.offset).toBe(1146);
  });
 });
